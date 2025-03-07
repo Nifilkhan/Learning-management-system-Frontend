79d8be461705702e2f9590fd450789d1e8801e6e
NifilKhan <nifilkhan.stackup@gmail.com>
2025-03-07 14:55:52 +0530

    video player error fixed

--- src/app/user/video-player/video-player.component.ts
+++ src/app/user/video-player/video-player.component.ts
@@ -4,6 +4,7 @@ import Player from "video.js/dist/types/player";
 import { Lecture, section } from '../shared/model/course';
 import { log } from 'console';
 import { threadId } from 'worker_threads';
+import { set } from 'video.js/dist/types/tech/middleware';
 
 @Component({
   selector: 'app-video-player',
@@ -14,12 +15,12 @@ import { threadId } from 'worker_threads';
 export class VideoPlayerComponent implements OnChanges,OnDestroy,AfterViewInit {
 
 
-  @ViewChild('videoElement', { static: false })videoElement!:ElementRef;
+  @ViewChild('videoElement', { static: false })videoElement?:ElementRef;
   @Input() selectedLecture!:Lecture;
   @Input() lectureData:section [] = []
   @Input() isFullScreen:boolean = false;
 
-  public player!:Player;
+  public player?:Player;
   isLoading:boolean = true;
 
   ngOnChanges(): void {
@@ -33,32 +34,34 @@ export class VideoPlayerComponent implements OnChanges,OnDestroy,AfterViewInit {
     }
 
     if(this.selectedLecture?.contentType === 'video' && this.selectedLecture.videoUrl) {
-      if(this.player) {
-        this.player.src({ type: 'video/mp4', src: this.selectedLecture.videoUrl });
-          console.log('player is not initialised...');
-          this.player.load();
-      } else {
-        setTimeout(() => {
-          this.initVideoPlayer();
-        }, 100);
-      }
+      this.isLoading = true;
+      setTimeout(() => {
+        if(this.player) {
+          this.player.src({ type: 'video/mp4', src: this.selectedLecture.videoUrl });
+            console.log('player is not initialised...');
+            this.player.load();
+        } else {
+            this.initVideoPlayer();
+        }
+        this.isLoading = false;
+      },400);
     }
   }
 
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit - Checking if player should initialize', this.selectedLecture);
-    if(this.selectedLecture && this.selectedLecture?.contentType === 'video'){
-      this.initVideoPlayer();
-    }
 
+    if(this.selectedLecture?.contentType === 'video'){
     setTimeout(() => {
+      this.initVideoPlayer();
       this.isLoading = false;
-    },4000)
+      },4000)
+    }
   }
 
   initVideoPlayer(): void {
     if(this.selectedLecture?.contentType === 'video') {
-      this.player = videojs(this.videoElement.nativeElement, {
+      this.player = videojs(this.videoElement?.nativeElement, {
         controls: true,
         autoplay: false,
         responsive: true,
